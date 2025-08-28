5a5a4e4342fa034f04f95d51b5d217b3096ce41c
JALMEJAY KUMAR SHAW <116828650+The-jalmejay@users.noreply.github.com>
2025-08-28 18:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let priceElement=document.querySelectorAll(".price")
+	let total=0;
+
+	priceElement.forEach(function(priceElement){
+		total+=parseFloat(priceElement.textContent);
+	})
+
+	const existingTotalRow = document.querySelector('#total-row');
+    if (existingTotalRow) {
+        existingTotalRow.remove();
+    }
+	const newRow = document.createElement('tr');
+    newRow.setAttribute('id', 'total-row');
+
+	const totalLabel = document.createElement('td');
+    totalLabel.textContent = 'Total = ';
+    const totalPriceCell = document.createElement('td');
+    totalPriceCell.textContent = total;
+	newRow.appendChild(totalLabel);
+    newRow.appendChild(totalPriceCell);
+    const table = document.querySelector('table');
+    table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
